6e5540269de92c8f7c46c6362d6a5ebc50ce6851
“gokulgowtham” <gokulgowtham001@gmail.com>
2025-02-22 01:46:30 +0530

    feat: map background added Add WorldMap component and integrate with Dashboard; update styles and dependencies

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import DashboardHeader from '../components/DashboardHeader'
+import React from "react";
+import DashboardHeader from "../components/DashboardHeader";
+import WorldMap from "../components/WorldMap";
 
 const Dashboard = () => {
   return (
-   <DashboardHeader />
+    <section className="DashboardContainer">
+      <DashboardHeader />
+      <div style={{ flex: 1, overflow: 'auto' }}> {/* Allow scrolling */}
+        <WorldMap />
+      </div>
+    </section>
   );
-}
+};
 
-export default Dashboard
+export default Dashboard;
