7671e8d688244765ab46a87976340a880cb34029
“gokulgowtham” <gokulgowtham001@gmail.com>
2025-02-21 19:33:21 +0530

    Initialize React project with Webpack and SCSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello, React with Webpack and SCSS!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your source index.html
+      filename: 'index.html', // Output file name in the dist folder
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
