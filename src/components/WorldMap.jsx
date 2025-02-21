a3c309c39f91cedcb3ad6e3171d9110c010ec3a9
Gowtham V <43110819+gokulgowtham@users.noreply.github.com>
2025-02-22 01:51:22 +0530

    Develop (#3)
    * Initialize React project with Webpack and SCSS setup
    
    * Add routing and dashboard components; integrate MUI AppBar and theme
    
    * feat: map background added
    Add WorldMap component and integrate with Dashboard; update styles and dependencies
    
    ---------
    
    Co-authored-by: “gokulgowtham” <“gokulgowtham001@gmail.com”>

---
+++ src/components/WorldMap.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from "react";
+import "./WorldMap.scss";
+import { Map } from "@maptiler/sdk";
+import "@maptiler/sdk/dist/maptiler-sdk.css"; // MapTiler CSS
+
+const WorldMap = () => {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    if (!mapContainer.current) return;
+
+    const map = new Map({
+      container: mapContainer.current,
+      style:
+        "https://api.maptiler.com/maps/4e843e4a-7638-4d1e-9613-3050e06fb6c9/style.json?key=dDyD8fg0ysJ2I6rb0wSF",
+      center: [0, 0], // Initial map center
+      zoom: 2, // Initial zoom level
+    });
+
+    // Remove zoom controls
+    map.removeControl("zoom");
+
+    // Cleanup on unmount
+    return () => map.remove();
+  }, []);
+
+  return (
+    <section className="mapContainer">
+      <div ref={mapContainer} style={{ height: "100vh", width: "100%" }} />
+    </section>
+  );
+};
+
+export default WorldMap;
