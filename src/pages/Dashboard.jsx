f89837d89e879765d8f2c94f37380b43decb05e5
“gokulgowtham” <gokulgowtham001@gmail.com>
2025-02-22 10:44:25 +0530

    feat: add CardWidget and DashboardContent components; enhance Dashboard layout and styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import DashboardHeader from "../components/DashboardHeader";
 import WorldMap from "../components/WorldMap";
 import "./Dashboard.scss";
+import { Typography } from "@mui/material";
+import DashboardContent from "../components/DashboardContent";
 const Dashboard = () => {
   return (
     <section className="DashboardContainer">
@@ -13,8 +15,8 @@ const Dashboard = () => {
           <WorldMap />
         </div>
         <div className="DashboardContent">
-          <h2>Dashboard Stats</h2>
-          <p>Coming soon...</p>
+          <DashboardContent />
+          {/* <p>Coming soon...</p> */}
         </div>
       </div>
     </section>
